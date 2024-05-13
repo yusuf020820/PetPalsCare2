60d6a8789a4a1fe6c2316c3d1c36a164390d5450
arfan0509 <arfanraja89@gmail.com>
2024-05-13 21:27:59 +0700

    awal

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline text-red-600">
       Hello world!
     </h1></>
   )
