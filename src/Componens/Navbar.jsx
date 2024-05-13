a65757f8761206f7e54066b86341805ac5a6a5bf
Yusuf Ardiansah <80136924+yusuf020820@users.noreply.github.com>
2024-05-13 21:22:04 +0700

    commit 1

--- src/Componens/Navbar.jsx
+++ src/Componens/Navbar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Hallo Nav</div>
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1></>
   )
 }
 
