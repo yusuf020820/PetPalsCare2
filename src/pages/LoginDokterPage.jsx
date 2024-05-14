f527d874cbc7b1334a683f54b69536607c9fb1d5
Yusuf Ardiansah <80136924+yusuf020820@users.noreply.github.com>
2024-05-15 00:27:47 +0700

    Login Dokter page part2

--- src/pages/LoginDokterPage.jsx
+++ src/pages/LoginDokterPage.jsx
@@ -2,19 +2,19 @@ import Logo from '../assets/images/logo.png'
 const LoginDokterPage = () => {
   return (
     <>
-      <div className="container flex justify-center items-center">
+      <div className="flex justify-center items-center">
         {/* kiri */}
         <div className="w-1/2 h-screen flex justify-center items-center" >
 
           <div className=" w-full p-20 text-center">
             <div className="text-lg w-auto h-auto font-semibold ">
               <h1 className="" >Masuk Sebagai Dokter Hewan</h1>
-              </div>
+            </div>
             <div className="py-4">
-              <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"placeholder="@email"/>
+              <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="@email" />
             </div>
             <div>
-              <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"placeholder="Kata Sandi"/>
+              <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Kata Sandi" />
             </div>
 
             <div className="p-4">
@@ -28,15 +28,15 @@ const LoginDokterPage = () => {
         {/* kanan */}
         <div className="w-1/2 h-screen bg-[#F7DBA7] flex justify-center items-center custom-border-radius">
           <div className=''>
-            <div className='h-auto w-full max-w-80 '>
+            <div className='h-auto w-auto max-w-80 '>
               <img src={Logo} alt="Logo" />
             </div>
 
             <div className='p-4 flex justify-center items-center'>
-            <button className=" w-auto h-auto bg-[#DE9455] hover:bg-[#D68B4B] text-white font-bold py-2 px-16 rounded-full font-semibold ">Masuk Sekarang</button>
+              <button className=" w-auto h-auto bg-[#DE9455] hover:bg-[#D68B4B] text-white font-bold py-2 px-16 rounded-full font-semibold ">Masuk Sekarang</button>
             </div>
             <div className='flex justify-center items-center '>
-            <a href="#" className='text-[#DE9455] hover: text-[#D68B4B]'>Masuk Sebagai Pemilik</a>
+              <a href="#" className='text-[#DE9455] hover: text-[#D68B4B]'>Masuk Sebagai Pemilik</a>
             </div>
           </div>
         </div>
