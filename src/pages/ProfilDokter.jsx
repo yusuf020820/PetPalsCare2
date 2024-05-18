a5e0b047639c98377b21be0d55f8199ca1199092
alfan122 <alfandimas089@gmail.com>
2024-05-18 11:08:03 +0700

    profildokter

--- src/pages/ProfilDokter.jsx
+++ src/pages/ProfilDokter.jsx
@@ -18,7 +18,7 @@ const ProfilDokter = () => {
         </div>
 
         <div className="bg-white w-full h-screen p-12">
-          <div className="bg-white p-10 rounded-xl w-full h-full shadow-2xl">
+          <div className="bg-white p-10 rounded-xl w-full h-auto shadow-2xl">
             <h1 className="py-4 font-bold text-1xl" > Profil Saya</h1>
             <div className="flex justify-start items-center gap-4 pt-6">
               <div className=" w-[100px] h-[100px] bg-orange-400 rounded-full flex justify-center items-center" >
@@ -30,15 +30,35 @@ const ProfilDokter = () => {
                 <p>Dokter Hewan</p>
               </div>
             </div>
-            <div classname="flex justify-start gap-8 items-center bg-red w-full" >
-              <div>
+            <div className=" h-1/2 flex justify-start gap-8 items-center  w-full h-auto gap-28">
+            <div className="py-4">
                 <p>Umur</p>
                 <p>27</p>
               </div>
-              <div>
+              <div className="py-4">
                 <p>Jenis Kelamin</p>
                 <p>Laki-laki</p>
               </div>
+            </div>
+            <div className="py-4">
+              <p>Nomor Handphone</p>
+              <p>+0123456789</p>
+            </div>
+            <div className="py-4">
+              <p>Email</p>
+              <p>muhammadAli</p>
+            </div>
+            <div className="py-4">
+              <p>Lulusan</p>
+              <p>Pendidikan Profesi Dokter</p>
+            </div>
+            <div className="py-4">
+              <p>Spesialis</p>
+              <p>Hewan domestik dan eksotik</p>
+            </div>
+            <div className="py-4">
+              <p>Biaya</p>
+              <p>Rp. 25.000,- </p>
             </div>
           </div>
         </div>
