2db5b2baf2c9f7bcb1764ac5c476139108b0e717
alfan122 <alfandimas089@gmail.com>
2024-05-20 08:05:22 +0700

    adopsiPage

--- src/pages/AdopsiPage.jsx
+++ src/pages/AdopsiPage.jsx
@@ -7,14 +7,14 @@ import Footer from '../Components/Footer'
 
 
 const data = [
-  { id: 1, JenisHewan: 'Anjing Shiba Inu', Nama: "Cleo", Kelamin: "Betina", Usia: 2, imageUrl: "https://via.placeholder.com/150" },
-  { id: 1, JenisHewan: "Anjing Shiba Inu", Nama: "Cleo", Kelamin: "Betina", Usia: 2, imageUrl: "https://via.placeholder.com/150" },
-  { id: 1, JenisHewan: "Anjing Shiba Inu", Nama: "Cleo", Kelamin: "Betina", Usia: 2, imageUrl: "https://via.placeholder.com/150" },
-  { id: 1, JenisHewan: "Anjing Shiba Inu", Nama: "Cleo", Kelamin: "Betina", Usia: 2, imageUrl: "https://via.placeholder.com/150" },
-  { id: 1, JenisHewan: "Anjing Shiba Inu", Nama: "Cleo", Kelamin: "Betina", Usia: 2, imageUrl: "https://via.placeholder.com/150" },
-  { id: 1, JenisHewan: "Anjing Shiba Inu", Nama: "Cleo", Kelamin: "Betina", Usia: 2, imageUrl: "https://via.placeholder.com/150" },
-  { id: 1, JenisHewan: "Anjing Shiba Inu", Nama: "Cleo", Kelamin: "Betina", Usia: 2, imageUrl: "https://via.placeholder.com/150" },
-  { id: 1, JenisHewan: "Anjing Shiba Inu", Nama: "Cleo", Kelamin: "Betina", Usia: 2, imageUrl: "https://via.placeholder.com/150" },
+  { id: 1, JenisHewan: 'Kucing Anggora', Nama: "Fluffy", Kelamin: "Betina", Usia: "2 Bulan", imageUrl: "https://i.ibb.co.com/ckN3v0F/Angora.png" },
+  { id: 1, JenisHewan: "Iguana Green", Nama: "Greendy", Kelamin: "Jantan", Usia: "9 Bulan", imageUrl: "https://i.ibb.co.com/MnCPpF6/iguana.png" },
+  { id: 1, JenisHewan: "Kelinci Alaska", Nama: "Bunny", Kelamin: "Betina", Usia: "6 Bulan", imageUrl: "https://i.ibb.co.com/WDQmvjH/kelinci.png" },
+  { id: 1, JenisHewan: "Kura-kura Darat", Nama: "Xeca", Kelamin: "Betina", Usia: "5 Tahun", imageUrl: "https://i.ibb.co.com/bFVrLxx/kura.png" },
+  { id: 1, JenisHewan: "Kucing Persia", Nama: "Sassy", Kelamin: "Betina", Usia: "2 Tahun", imageUrl: "https://i.ibb.co.com/K98b2mf/persia.png" },
+  { id: 1, JenisHewan: "Anjing Shiba Inu", Nama: "Cleo", Kelamin: "Betina", Usia: "2 Tahun", imageUrl: "https://i.ibb.co.com/ct4kv3b/shiba.png" },
+  { id: 1, JenisHewan: "Kucing Lokal", Nama: "(Belum Ada)", Kelamin: "Jantan", Usia: "2 Bulan", imageUrl: "https://i.ibb.co.com/c8kJ9x9/lokal.png" },
+  { id: 1, JenisHewan: "Anjing Maltase", Nama: "Fuzzy", Kelamin: "Betina", Usia: "3 Bulan", imageUrl: "https://i.ibb.co.com/wSh5zGx/maltase.png" },
 ]
 
 
@@ -30,7 +30,7 @@ const Card = ({ JenisHewan, Nama, Kelamin, Usia, imageUrl }) => {
         <p className="text-[#667479] text-base">Nama : {Nama}</p>
         <div className=' flex gap-2'>
           <p className="text-[#667479] text-xs">Kelamin : {Kelamin}</p>
-          <p className="text-[#667479] text-xs"> Usia : {Usia} tahun</p>
+          <p className="text-[#667479] text-xs"> Usia : {Usia}</p>
         </div>
       </div>
     </div>
@@ -42,11 +42,11 @@ const AdopsiPage = () => {
     <>
       <Navbar />
       <div className=' w-full h-auto pt-8 px-20 container'>
-        <div className='bg-green h-96 w-full'>
-          <img src="" alt="" />
+        <div>
+          <img src="adopsi.png" alt="adopsi" className='px-30'/>
         </div>
 
-        <div>
+        <div >
           <div className='py-8 flex gap-4'>
             <div className="container mx-auto p-4">
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
