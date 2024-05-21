174a07eecf5c0c4e60bf3ed75f10153e3cfc0795
Yusuf Ardiansah <80136924+yusuf020820@users.noreply.github.com>
2024-05-21 22:10:34 +0700

    revisi dikit 2

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -20,7 +20,9 @@ const LoginPage = () => {
             </div>
 
             <div className="p-4">
-              <button className=" w-auto h-auto bg-[#DE9455] hover:bg-[#D68B4B] text-white font-bold py-2 px-16 rounded-full font-semibold size-1 ">Masuk Sekarang</button>
+              <button className=" w-auto h-auto bg-[#DE9455] hover:bg-[#D68B4B] text-white font-bold py-2 px-16 rounded-full font-semibold size-1 ">
+                <a href="/Beranda">Masuk Sekarang</a>
+                </button>
             </div>
           </div>
 
