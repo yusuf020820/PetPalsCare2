318a7f18081d066c9279719aacbfe5bf3a303278
arfan0509 <arfanraja89@gmail.com>
2024-05-31 22:58:40 +0700

    push

--- frontend/src/pages/DetailDokterCowo.jsx
+++ frontend/src/pages/DetailDokterCowo.jsx
@@ -1,16 +1,17 @@
-import DokterCowo from '../assets/images/DokterCowo.png'
-import React from 'react';
-import Footer from '../Components/Footer-after';
+import DokterCowo from "../assets/images/DokterCowo.png";
+import React from "react";
+import Footer from "../Components/Footer-after";
 import Navbar from "../Components/Navbar-after";
 
 const posts = [
   {
     id: 1,
-    title: 'Drh. Muhammad Ali',
-    href: '#',
-    description: 'KEDIRI',
+    title: "Drh. Muhammad Ali",
+    href: "#",
+    description: "KEDIRI",
     author: {
-      imageUrl: 'https://drive.google.com/uc?id=1oonn5IChr3VGrSBLD2Pg853o18FPEvbu', // Ganti dengan URL gambar yang benar
+      imageUrl:
+        "https://drive.google.com/uc?id=1oonn5IChr3VGrSBLD2Pg853o18FPEvbu", // Ganti dengan URL gambar yang benar
     },
   },
   // More posts...
@@ -25,10 +26,17 @@ const Example = () => {
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-1">
           {posts.map((post) => (
-            <article key={post.id} className="flex flex-col items-start justify-between bg-white shadow-lg rounded-lg p-6 w-full">
+            <article
+              key={post.id}
+              className="flex flex-col items-start justify-between bg-white shadow-lg rounded-lg p-6 w-full"
+            >
               <div className="flex flex-col sm:flex-row sm:items-start sm:gap-x-6 mt-3 w-full">
                 {post.author.imageUrl && (
-                  <img src={DokterCowo} alt="" className="h-40 w-40 rounded-full bg-gray-50" />
+                  <img
+                    src={DokterCowo}
+                    alt=""
+                    className="h-40 w-40 rounded-full bg-gray-50"
+                  />
                 )}
                 <div className="flex flex-col mt-3 sm:mt-0 w-full">
                   <h3 className="text-lg font-semibold leading-6 text-gray-900">
@@ -37,18 +45,39 @@ const Example = () => {
                       {post.title}
                     </a> */}
                   </h3>
-                  <p className="mt-5 text-sm leading-6 text-gray-600">{post.description}</p>
                   <p className="mt-5 text-sm leading-6 text-gray-600">
-                    <span style={{ color: '#564741' }}><strong>Nama:</strong></span> {post.title} <br />
-                    <span style={{ color: '#564741' }}><strong>Region/Wilayah:</strong></span> Kediri <br />
-                    <span style={{ color: '#564741' }}><strong>Lulusan:</strong></span> Pendidikan Profesi Dokter Hewan, Fakultas Kedokteran Hewan, Universitas Syiah Kuala <br />
-                    <span style={{ color: '#564741' }}><strong>Pengalaman:</strong></span> 7 Tahun <br />
-                    <span style={{ color: '#564741' }}><strong>Spesialis Hewan:</strong></span> Hewan Domestik dan Eksotik
+                    {post.description}
+                  </p>
+                  <p className="mt-5 text-sm leading-6 text-gray-600">
+                    <span style={{ color: "#564741" }}>
+                      <strong>Nama:</strong>
+                    </span>{" "}
+                    {post.title} <br />
+                    <span style={{ color: "#564741" }}>
+                      <strong>Region/Wilayah:</strong>
+                    </span>{" "}
+                    Kediri <br />
+                    <span style={{ color: "#564741" }}>
+                      <strong>Lulusan:</strong>
+                    </span>{" "}
+                    Pendidikan Profesi Dokter Hewan, Fakultas Kedokteran Hewan,
+                    Universitas Syiah Kuala <br />
+                    <span style={{ color: "#564741" }}>
+                      <strong>Pengalaman:</strong>
+                    </span>{" "}
+                    7 Tahun <br />
+                    <span style={{ color: "#564741" }}>
+                      <strong>Spesialis Hewan:</strong>
+                    </span>{" "}
+                    Hewan Domestik dan Eksotik
                   </p>
                   <div className="mt-5 flex space-x-4">
-                  <button className="px-4 py-2 bg-[#ED9455] text-white rounded-lg hover:bg-[#d8854c]">
-                    <a href="/Pop-Up-Konfirmasi">Konsultasi</a></button>
-                    <button className="px-4 py-2 bg-[#ED9455] text-white rounded-lg hover:bg-[#d8854c]">Lokasi Praktik</button>
+                    <button className="px-4 py-2 bg-[#ED9455] text-white rounded-lg hover:bg-[#d8854c]">
+                      <a href="/Pop-Up-Konfirmasi">Konsultasi</a>
+                    </button>
+                    <button className="px-4 py-2 bg-[#ED9455] text-white rounded-lg hover:bg-[#d8854c]">
+                      Lokasi Praktik
+                    </button>
                   </div>
                 </div>
               </div>
@@ -65,8 +94,8 @@ const DetailDokterCowo = () => {
     <>
       <Navbar />
       <Example />
-      <Footer/>
-      </>
+      <Footer />
+    </>
   );
 };
 
